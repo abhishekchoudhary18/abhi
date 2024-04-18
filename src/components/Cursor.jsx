e0023cb46b428b594c6f6dc0645adc37cecd710d
abhishekchoudhary18 <abhishekchoudhary18898@gmail.com>
2024-04-19 00:59:35 +0530

    unrelated class removed

--- src/components/Cursor.jsx
+++ src/components/Cursor.jsx
@@ -1,37 +1,47 @@
 import { useEffect } from "react";
-import {useCursor} from "../context/CursorProvider";
+import { useCursor } from "../context/CursorProvider";
 
-export default function Cursor (){
+export default function Cursor() {
+  const { cursorDotref, cursorCircleref, cursorTextref, cursorText } =
+    useCursor();
 
-    const {cursorDotref,cursorCircleref,cursorTextref,cursorText} = useCursor()
+  const movingText = cursorText.split("").map((char, i) => {
+    return (
+      <span
+        key={i}
+        style={{ transform: `rotate(${i * (340 / cursorText.length)}deg)` }}
+      >
+        {char}
+      </span>
+    );
+  });
 
-    const movingText = cursorText.split("").map((char,i)=>{
-        return <span key={i} style={{transform:`rotate(${i*(340/cursorText.length) }deg)`}}>{char}</span>
-    })
+  useEffect(() => {
+    const mouseMove = (e) => {
+      cursorDotref.current.style.left = `${e.clientX}px`;
+      cursorDotref.current.style.top = `${e.clientY}px`;
 
-    useEffect(()=>{
-        const mouseMove = (e) => {
-            cursorDotref.current.style.left = `${e.clientX}px`;
-            cursorDotref.current.style.top = `${e.clientY}px`;
+      cursorTextref.current.style.left = `${e.clientX - 50}px`;
+      cursorTextref.current.style.top = `${e.clientY - 50}px`;
 
-            cursorTextref.current.style.left = `${e.clientX-50}px`;
-            cursorTextref.current.style.top = `${e.clientY-50}px`;
+      cursorCircleref.current.animate(
+        {
+          left: `${e.clientX}px`,
+          top: `${e.clientY}px`,
+        },
+        { duration: 5000, fill: "forwards" }
+      );
+    };
+    window.addEventListener("mousemove", mouseMove);
+  }, []);
 
-            cursorCircleref.current.animate({
-                left:`${e.clientX}px`,
-                top:`${e.clientY}px`
-            },{duration:5000,fill:"forwards"});
-
-            
-        }
-        window.addEventListener("mousemove",mouseMove)
-    },[])
-
-    return<>
-        <div className="cursor-dot logo"  ref={cursorDotref}></div>
-        <div className="cursor-outline" ref={cursorCircleref}></div>
-        <div className="cursor-text" ref={cursorTextref}>
-            <div className="text" >{movingText}</div>
-        </div>
+  return (
+    <>
+      <div className="cursor-dot" ref={cursorDotref}></div>
+      <div className="cursor-outline" ref={cursorCircleref}></div>
+      <div className="cursor-text" ref={cursorTextref}>
+        <div className="text">{movingText}</div>
+      </div>
     </>
+  );
 }
